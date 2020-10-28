d4c79ed933ff6cc1f760948af11723233fdc63bc
MarcosBeerg <marquinhos.mlpc@gmail.com>
2020-10-28 11:13:06 -0700

    new commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,17 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Product;
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    private $product;
+
+    public function __construct (Product $product)
     {
-        $this->middleware('auth');
+        $this->product = $product;
     }
 
     /**
@@ -23,6 +20,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products = $this->product->limit(8)->get();
+
+        return view('welcome',compact('products'));
+
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,17 @@
 
 
 @section('content')
-
+@foreach($products as $product)
+    <div class="card">
+        @if($product->photos->count())
+        <img src="{{$product->photos->first()->image}}" alt="" class="card-img-top">
+        @endif
+        <div class="card-body">
+            <h2 class="card-title">{{$product->name}}</h2>
+            <p class="card-text">
+            {{$product->description}}
+            </p>
+        </div>
+    </div>
+    @endforeach
 @endsection
