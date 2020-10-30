fdb66bd7f24aa87f0ce773cc3c48254347bea80d
MarcosBeerg <marquinhos.mlpc@gmail.com>
2020-10-30 15:03:01 -0700

    remove purchase

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -45,4 +45,14 @@ class CartController extends Controller
         session()->put('cart', $products);
         return redirect()->route('cart.index');
     }
+
+    public function cancel()
+    {
+            session()->forget('cart');
+
+            flash('Compra cancelada !')->success();
+            return redirect()->route('cart.index');   
+    
+
+    }
 }
