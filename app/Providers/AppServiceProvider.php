5f81218911c8cdb2332c6b5594e2a169ed5105ae
MarcosBeerg <marquinhos.mlpc@gmail.com>
2020-11-04 15:36:57 -0800

    card flag

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \PagSeguro\Library::initialize();
+        \PagSeguro\Library::cmsVersion()->setName("Marketplace")->setRelease("1.0.0");
+        \PagSeguro\Library::moduleVersion()->setName("Marketplace")->setRelease("1.0.0");
+
     }
 }
