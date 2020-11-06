839500520036b50512e665489216b8ff8ebc939f
MarcosBeerg <marquinhos.mlpc@gmail.com>
2020-11-06 15:59:28 -0800

    notification method

--- app/User.php
+++ app/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         return $this->hasOne(Store::class);
     }
 
+    public function orders()
+    {
+        return $this->hasMany(UserOrder::class);
+    }
 }
